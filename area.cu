afa08ad84222c63c56682dd75357edda0843e40c
Savcab <felixchenyijun@gmail.com>
2024-04-27 17:42:07 -0700

    half done with grid method

--- area.cu
+++ area.cu
@@ -3,9 +3,14 @@
 #include <cublas.h>
 #include <time.h>
 #include <fstream>
+#include <thrust/sort.h>
+#include <thrust/device_vector.h>
+#include <thrust/fill.h>
+
+#define INT_MAX 2147483647
 
 #define spaceSize 10000
-#define numBoids  128//has to be multiple of blockSize
+#define numBoids  128
 #define numIters 5000
 #define visualRange 10000
 #define boidMass 1
@@ -15,8 +20,13 @@
 #define repulsionWeight 0.5
 #define alignmentWeight 0.05
 
-#define dimBlock 32
-#define dimGrid (numBoids / dimBlock)
+// Blocks and grids are 2D now, these are parameters along one of the edges
+#define grid1Dim 10
+#define block1Dim 10
+
+// Dimensions for finding the Start & End for boids in areaIds
+#define gridDimSE 10
+#define blockDimSE 10
 
 void checkCudaError(std::string str)
 {
@@ -41,6 +51,29 @@ struct Force
     double Fx;
     double Fy;
 };
+
+// For convienience
+struct BoidsContext
+{
+    Boid* boids;
+    int* startIdx;
+    int* endIdx;
+}
+
+// Helper function to return which area this boid belongs to
+__device__ __host__ int areaId(Boid& boid)
+{
+    int numAreas1D = block1Dim * grid1Dim;
+    int areaSide = spaceSize / numAreas1D;
+    return ((int)boid.x / areaSide) + numAreas1D * ((int)boid.y / areaSide);
+}
+
+// Helper function to convert xy thread coordinate to areaId
+__device__ int areaId(int myThreadX, int myThreadY)
+{
+    int numAreas1D = block1Dim * grid1Dim;
+    return myThreadX + myThreadY * numAreas1D;
+}
 
 // Helper function to calculate distance between two boids
 __device__ double calcDist(Boid& b1, Boid& b2)
@@ -184,20 +217,85 @@ __device__ void updateBoid(Boid& boid)
     boid.yAcc = 0;
 }
 
-// One step of naive: calculate the acceleration of each boid. DOESN'T apply it yet
-__global__ void naiveCalcAcc(Boid* boids, int nBoids)
+// NOTE: 
+//  startIdx is inclusive
+//  endIdx is exclusive
+__global__ void parallelFindStartEnd(Boid* boids, int* startIdx, int* endIdx)
 {
-    int currIdx = blockIdx.x * blockDim.x + threadIdx.x;
-    Boid boidCpy = boids[currIdx];
-    // Center attarction force
-    // NOTE: could probably optimize by making memory accesses closer to each other
-    applyCenterAttr(boidCpy, currIdx, boids, nBoids);
-    applyAvoidOthers(boidCpy, currIdx, boids, nBoids);
-    applyAlignment(boidCpy, currIdx, boids, nBoids);
-    boids[currIdx] = boidCpy;
+    int threadId = blockIdx.x * blockDim.x + threadIdx.x;
+    int startingIdx = numBoids * threadId / (gridDim.x * blockDim.x);
+    int endingIdx  = numBoids * (threadId + 1) / (gridDim.x * blockDim.x);
+    // Initialize the conditions
+    int prevVal = areaId(boids[startingIdx]);
+    atomicMin(&(startIdx[prevVal]), startingIdx);
+    atomicMax(&(endIdx[prevVal]), startingIdx);
+    // Iterating through the whole thing linearly
+    // CAN BE OPTIMIZED BY USING BINARY SEARCH
+    for(int i = startingIdx + 1; i < endingIdx; i++)
+    {
+        int currVal = areaId(boids[i]);
+        if(currVal != prevVal)
+        {
+            atomicMin(&(startIdx[currVal]), i);
+            atomicMax(&(endIdx[currVal]), i);
+            prevVal = currVal;
+        }
+    }
 }
 
-__global__ void naiveUpdateBoids(Boid* boids, int nBoids)
+// One step of naive: calculate the acceleration of each boid. DOESN'T apply it yet
+__global__ void areaCalcAcc(Boid* boids, int* startIdx, int* endIdx)
+{
+    int currX = blockIdx.x * blockDim.x + threadIdx.x;
+    int currY = blockIdx.y * blockDim.y + threadIdx.y;
+    int aId = areaId(currX, currY);
+    // Edge case: empty area
+    if(startIdx[aId] == INT_MAX && endIdx[aId] == -1)
+    {
+        return;
+    }
+
+    // Find areas around it that we need to inlude in calculations
+    // L = neighborhood radius(neighborhood is a square block)
+    int L = visualRange / (spaceSize / (grid1Dim * block1Dim)) + 1;
+    int neighArea = (2*L+1) * (2*L+1);
+    int neighIds[neighArea];
+    int count = 0;
+    for(int y = currY - L; y <= currY + L; y++)
+    {
+        for(int x = currX - L; x <= currX + L; x++)
+        {
+            neighIds[count] = areaId(x, y);
+            count++;
+        }
+    }
+
+    // Iterate through all boids in this area and do all calculations
+    for(int i = startIdx[aId]; i < endIdx[aId]; i++)
+    {
+        Boid boidCpy = boids[i];
+        // Itearte through whole neighborhood to calculate forces
+        for(int nIdx = 0; nIdx < neighArea; nIdx++)
+        {
+            // TODO: need to fix currIdx thing!
+            int nStartIdx = startIdx[neighIds[nIdx]];
+            int nEndIdx = endIdx[neighIds[nIdx]];
+            int nBoids = nEndIdx - nStartIdx;
+            if(nStartIdx == INT_MAX && nEndIDx == -1)
+            {
+                continue;
+            }
+            Boid* nStartPtr = boids + nStartIdx;
+            applyCenterAttr(boidCpy, currIdx, nStartPtr, nBoids);
+            applyAvoidOthers(boidCpy, currIdx, nStartPtr, nBoids);
+            applyAlignment(boidCpy, currIdx, nStartPtr, nBoids);
+        }
+        boids[currIdx] = boidCpy;
+    }
+}
+
+// TODO: need to implement
+__global__ void areaUpdateBoids(Boid* boids, int* startIdx, int* endIdx)
 {
     int currIdx = blockIdx.x * blockDim.x + threadIdx.x;
     Boid boidCpy = boids[currIdx];
@@ -205,28 +303,44 @@ __global__ void naiveUpdateBoids(Boid* boids, int nBoids)
     boids[currIdx] = boidCpy;
 }
 
+// Customer comparator functor for sorting boids according to which area they land in
+struct CompareByAreaId {
+    __host__ __device__
+    bool operator()(const Boid& boid1, const Boid& boid2) const
+    {
+        int aId1 = areaId(boid1);
+        int aId2 = areaId(boid2);
+        return aId1 <= aId2;
+    }
+}
+
 int main(int argc, char **argv)
 {
     // Allocate memory for host
-    Boid *boids;
-    // Paged-locked memory doesn't get swapped back to disk
-    cudaMallocHost((void**)&boids, sizeof(Boid) * numBoids);
+    thrust::host_vector<Boid> boids;
 	
     // Initialize boids
     int gap = spaceSize / numBoids;
     for(int i = 0; i < numBoids; i++)
     {
-        boids[i].x = i * gap;
-        boids[i].y = i * gap;
-        boids[i].xVel = rand() % 20 - 10;
-        boids[i].yVel = rand() % 20 - 10;
-        boids[i].xAcc = 0;
-        boids[i].yAcc = 0;
+        Boid temp;
+        temp.x = i * gap;
+        temp.y = i * gap;
+        temp.xVel = rand() % 20 - 10;
+        temp.yVel = rand() % 20 - 10;
+        temp.xAcc = 0;
+        temp.yAcc = 0;
+        boids.push_back(temp);
     }
 
     // Allocate memory for device
-    Boid* gpu_boids;
-    cudaMalloc(&gpu_boids, sizeof(Boid) * numBoids);
+    thrust::device_vector<Boid> gpu_boids = boids;
+    thrust::device_vector<int> gpu_startIdx(grid1Dim * block1Dim * grid1Dim * block1Dim, INT_MAX);
+    thrust::device_vector<int> gpu_endIdx(grid1Dim * block1Dim * grid1Dim * block1Dim, -1);
+
+    Boid* gpu_boidsPtr = thrust::raw_pointer_cast(gpu_boids.data());
+    int* gpu_startIdxPtr = thrust::raw_pointer_cast(gpu_startIdx.data());
+    int* gpu_endIdxPtr = thrust::raw_pointer_cast(gpu_endIdx.data());
     
     // start time
     struct timespec start, stop; 
@@ -235,17 +349,25 @@ int main(int argc, char **argv)
     std::ofstream oTestFile("test.txt");
 
     // Start calling the gpu
-    // cudaEventRecord(startEvent, 0);
-    dim3 dimBlock(dimBlock);
-    dim3 dimGrid(dimGrid);
+    dim3 dimBlock(block1Dim, block1Dim);
+    dim3 dimGrid(grid1Dim, grid1Dim);
+
+    dim3 dimBlockLinear(block1Dim * block1Dim);
+    dim3 dimGridLinear(grid1Dim * grid1Dim);
     // Run all the timesteps
     if( clock_gettime( CLOCK_REALTIME, &start) == -1 ) { perror( "clock gettime" );}
-    cudaMemcpy(gpu_boids, boids, sizeof(Boid) * numBoids, cudaMemcpyHostToDevice);
     for(int i = 0; i < numIters; i++)
     {
-        naiveCalcAcc <<< dimGrid, dimBlock >>> (gpu_boids, numBoids);
-        checkCudaError("After naiveCalcAcc");
-        naiveUpdateBoids <<< dimGrid, dimBlock >>> (gpu_boids, numBoids);
+        // Sort the boids such that boids in the same area are next to each other
+        thrust::sort(gpu_boids.begin(), gpu_boids.end(), CompareByAreaId());
+        // Find start and end indices in "boids" for every area 
+        thrust::fill(gpu_startIdx.begin(), gpu_startIdx.end(), INT_MAX);
+        thrust::fill(gpu_endIdx.begin(), gpu_endIdx.end(), -1);
+        parallelFindStartEnd <<< dimGridLinear, dimBlockLinear >>> (gpu_boidsPtr, gpu_startIdxPtr, gpu_endIdxPtr);
+
+        areaCalcAcc <<< dimGrid, dimBlock >>> (gpu_boidsPtr, gpu_startIdxPtr, gpu_endIdxPtr);
+        checkCudaError("After areaCalcAcc");
+        areaUpdateBoids <<< dimGrid, dimBlock >>> (gpu_boidsPtr, gpu_startIdxPtr, gpu_endIdxPtr);
         checkCudaError("After naiveUpdateBoids");
         cudaMemcpy(boids, gpu_boids, sizeof(Boid) * numBoids, cudaMemcpyDeviceToHost);
         checkCudaError("After cudaMemcpy device to host");
